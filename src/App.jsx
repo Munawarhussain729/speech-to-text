becd6f3d235b462fa8833b830187f21ff45200b7
Munawar Hussain <munawarhussa.7@gmail.com>
2024-04-30 18:28:24 +0500

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
+
 import "regenerator-runtime/runtime"
 import './App.css'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useState } from "react";
 
 function App() {
-  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-
+  const [message, setMessage] = useState('')
+  const commands = [
+    {
+      command: 'I would like to order *',
+      callback: (food) => setMessage(`Your order is for: ${food}`)
+    },
+    {
+      command: 'The weather is :condition today',
+      callback: (condition) => setMessage(`Today, the weather is ${condition}`)
+    },
+    {
+      command: 'My top sports are * and *',
+      callback: (sport1, sport2) => setMessage(`#1: ${sport1}, #2: ${sport2}`)
+    },
+    {
+      command: 'Pass the salt (please)',
+      callback: () => setMessage('My pleasure')
+    },
+    {
+      command: ['Hello', 'Hi'],
+      callback: ({ command }) => setMessage(`Hi there! You said: "${command}"`),
+      matchInterim: true
+    },
+    {
+      command: 'Beijing',
+      callback: (command, spokenPhrase, similarityRatio) => setMessage(`${command} and ${spokenPhrase} are ${similarityRatio * 100}% similar`),
+      // If the spokenPhrase is "Benji", the message would be "Beijing and Benji are 40% similar"
+      isFuzzyMatch: true,
+      fuzzyMatchingThreshold: 0.2
+    },
+    {
+      command: ['eat', 'sleep', 'leave'],
+      callback: (command) => setMessage(`Best matching command: ${command}`),
+      isFuzzyMatch: true,
+      fuzzyMatchingThreshold: 0.2,
+      bestMatchOnly: true
+    },
+    {
+      command: 'clear',
+      callback: ({ resetTranscript }) => resetTranscript()
+    }
+  ]
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands });
 
   const startListening = () => {
     SpeechRecognition.startListening({ continuous: true, language: 'en-IN' })
@@ -19,11 +62,12 @@ function App() {
   return (
     <>
       <h1>Speech to Text</h1>
+      <p>Message is: {message}</p>
       <div className='main-content'>
         {transcript}
       </div>
-      <div>
-        <button>Copy</button>
+      <div className="button-container">
+        <button>Copy to clipboard</button>
         <button onClick={startListening}>Start</button>
         <button onClick={stopListening}>Stop</button>
       </div>
